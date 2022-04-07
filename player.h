d95d843c91a2c685f3d635d4ea0b1ecae20b5263
Randy Do <randydo@Randys-MBP.socal.rr.com>
2022-04-06 18:50:23 -0700

    player class and draw function completed

--- player.h
+++ player.h
@@ -1,2 +1,25 @@
 // Similar to opponent.h (look there for reference)
+#ifndef PLAYER_H
+#define PLAYER_H
+
+#include "cpputils/graphics/image.h"
+#include <string>
+class Player{
+    private:
+        int x_, y_;
+        const int kWidth = 50;
+        const int kHeight = 50;
+    public:
+        Player();
+        Player(int x, int  y);
+        void SetX(int x);
+        void SetY(int y);
+        int GetX();
+        int GetY();
+        int GetWidth();
+        int GetHeight();
+        void DrawPlayerImage(std::string player_image_file);
+        void Draw(graphics::Image& gameScreen);
+};
+#endif
 
